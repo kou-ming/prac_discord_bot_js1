2916671a780fa2a1f424f9592b05e66df2c3c802
kou-ming <t0937993806@gmail.com>
2024-04-27 22:09:10 +0800

    [EP6]完成ping

---
+++ src/events/ready/index.js
@@ -0,0 +1,11 @@
+import {Events} from 'discord.js';
+
+export const event = {
+    name: Events.ClientReady,
+    once: true
+}
+
+export const action = (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(c.guilds);
+}
