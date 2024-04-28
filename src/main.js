6287dc391fc1246ecc4649dd64b46271fb2f71be
kou-ming <t0937993806@gmail.com>
2024-04-28 20:44:46 +0800

    [428844]使用回復來抓取要上傳的圖片

--- src/main.js
+++ src/main.js
@@ -12,11 +12,19 @@ dotenv.config();
 const appStore = useAppStore();
 
 // 新增客戶端的實體
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ] 
+});
 
 appStore.client = client;
 
 // 載入事件，在ready事件載入指令
 loadEvents();
+
 
 client.login(process.env.TOKEN);
